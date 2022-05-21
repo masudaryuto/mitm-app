466591c56daec73291b46c852100617bd615a832
masudaryuto <masuda200217@icloud.com>
2022-05-22 00:32:37 +0900

    update VCompany.vue ver4.0

--- src/components/mitm-app/VCompany.vue
+++ src/components/mitm-app/VCompany.vue
@@ -8,24 +8,29 @@
 	</div>
 	</v-layout>
 
-	<br><br>
+	<br>
 
 	
 	<v-row
 		align="center"
 		justify="space-around"
 		class="ma-n2"
+		id="row-label"
 	>
 		<v-chip
 			class="Indust"
 			label
+			x-large
+			id="box"
 		>
-
-			{{industryText}}
+		{{industryText}}
+			
 		</v-chip>
 		<v-chip
 			class="status"
 			label
+			x-large
+			id="box"
 		>
 			{{statusText}}
 		</v-chip>
@@ -45,17 +50,17 @@
 			outlined
 			title
 			>
-			<div class="schedule_title">
+			<div class="schedule_title" id="schedule-title">
 				<p>{{companySchedule.scheduleTitle}}</p>
 			</div>
 			<div class="schedule_date">
-				<p>{{companySchedule.scheduleDate}}</p>
+				<pre>日付&#009;&#009;{{companySchedule.scheduleDate}}</pre>
 			</div>
 			<div class="company_url">
-				<p>{{companySchedule.scheduleUrl}}</p>
+				<pre>URL&#009;&#009;<a v-bind:href="companySchedule.scheduleUrl">{{companySchedule.scheduleUrl}}</a></pre>
 			</div>
 			<div class="memo">
-				<p>{{companySchedule.scheduleMemo}}</p>
+				<pre>メモ&#009;&#009;{{companySchedule.scheduleMemo}}</pre>
 			</div>
 		</v-card>
 
@@ -77,9 +82,9 @@ export default class VCompany extends Vue {
 	public industryText = '業種'
 	public company_url = 'https://...'
 	//public companySchedule = {scheduleTitle:'予定タイトル', scheduleDate:'日付', scheduleUrl:'URL', scheduleMemo:'メモ'}
-	
+	public memo = '業界研究１DAYインターンシップは、半日プログラム（２時間程度）です。 総合職のマーケティングや 企画の体験を通じて、業界研究ができるセミナーです。少人数での開催ですので、人事担当者に業界の事や就職活動の事等何でもご相談いただけます。お気軽にご参加お待ちしています♪'
 	public companySchedules = [
-		{scheduleTitle:'予定タイトル', scheduleDate:'日付', scheduleUrl:'URL', scheduleMemo:'メモ'},
+		{scheduleTitle:'1day業界セミナー', scheduleDate:'2022年5月21日', scheduleUrl:'https://talent.supporterz.jp/events/', scheduleMemo:this.memo},
 		{scheduleTitle:'予定タイトル', scheduleDate:'日付', scheduleUrl:'URL', scheduleMemo:'メモ'},
 		{scheduleTitle:'予定タイトル', scheduleDate:'日付', scheduleUrl:'URL', scheduleMemo:'メモ'},
 	]
@@ -97,5 +102,33 @@ export default class VCompany extends Vue {
 	font-size: xx-large;
 	//font-family:'Times New Roman', Times, serif;
 }
+
+#row-label{
+	
+}
+
+#box{
+	//width: 200px;
+	padding-left: 10%;
+	padding-right: 10%;
+
+}
+
+.v-chip__content{
+
+
+	
+}
+
+#schedule-title{
+	font-size: 24px;
+	font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+
+}
+#schedule-title > p{
+	margin-bottom: 0%;
+}
+
+
 
 </style>
