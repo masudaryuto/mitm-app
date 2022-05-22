1990bb86db54522a7ee817acb3c2151cce5b7a04
thiraro <thirathiracat@gmail.com>
2022-05-22 13:27:12 +0900

    VCalendar was fixed.

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 
 import axios from 'axios' //追加
-import VueAxios from 'vue-axios' //追加
+//import VueAxios from 'vue-axios' //追加
 
 
 Vue.config.productionTip = false
 
-Vue.use(VueAxios, axios) //追加
+//Vue.use(VueAxios, axios) //追加
 
 new Vue({
   router,
